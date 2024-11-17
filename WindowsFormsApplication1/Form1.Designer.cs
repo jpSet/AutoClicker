b9932eb12a77951f7790517a7e2be28811626a5c
jpalm <jpvalmeida@gmail.com>
2024-11-17 18:32:34 +0000

    Add project files.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,344 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rb_Universo = new System.Windows.Forms.RadioButton();
+            this.rb_Galaxia = new System.Windows.Forms.RadioButton();
+            this.btn_Pos1 = new System.Windows.Forms.Button();
+            this.btn_Pos2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cb_clickTempo = new System.Windows.Forms.ComboBox();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.timerPoint1 = new System.Windows.Forms.Timer(this.components);
+            this.timerPoint2 = new System.Windows.Forms.Timer(this.components);
+            this.lbl_P1 = new System.Windows.Forms.Label();
+            this.lbl_P2 = new System.Windows.Forms.Label();
+            this.btn_pos3 = new System.Windows.Forms.Button();
+            this.lbl_avancaSS = new System.Windows.Forms.Label();
+            this.lbl_avancaGalax = new System.Windows.Forms.Label();
+            this.lbl_posBtnVer = new System.Windows.Forms.Label();
+            this.btn_pos4 = new System.Windows.Forms.Button();
+            this.lbl_recuaSS = new System.Windows.Forms.Label();
+            this.lbl_actualizaGalax = new System.Windows.Forms.Label();
+            this.cb_actualizaGalax = new System.Windows.Forms.ComboBox();
+            this.timePoint3 = new System.Windows.Forms.Timer(this.components);
+            this.timerPoint4 = new System.Windows.Forms.Timer(this.components);
+            this.timerAvancaSS = new System.Windows.Forms.Timer(this.components);
+            this.timerRecuaSS = new System.Windows.Forms.Timer(this.components);
+            this.timerPressionaBTver = new System.Windows.Forms.Timer(this.components);
+            this.timerAvancaGalaxia = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rb_Universo);
+            this.groupBox1.Controls.Add(this.rb_Galaxia);
+            this.groupBox1.Location = new System.Drawing.Point(87, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(90, 64);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Modo";
+            // 
+            // rb_Universo
+            // 
+            this.rb_Universo.AutoSize = true;
+            this.rb_Universo.Location = new System.Drawing.Point(17, 41);
+            this.rb_Universo.Name = "rb_Universo";
+            this.rb_Universo.Size = new System.Drawing.Size(67, 17);
+            this.rb_Universo.TabIndex = 1;
+            this.rb_Universo.TabStop = true;
+            this.rb_Universo.Text = "Universo";
+            this.rb_Universo.UseVisualStyleBackColor = true;
+            this.rb_Universo.CheckedChanged += new System.EventHandler(this.rb_Universo_CheckedChanged);
+            // 
+            // rb_Galaxia
+            // 
+            this.rb_Galaxia.AutoSize = true;
+            this.rb_Galaxia.Location = new System.Drawing.Point(17, 18);
+            this.rb_Galaxia.Name = "rb_Galaxia";
+            this.rb_Galaxia.Size = new System.Drawing.Size(60, 17);
+            this.rb_Galaxia.TabIndex = 0;
+            this.rb_Galaxia.TabStop = true;
+            this.rb_Galaxia.Text = "Galaxia";
+            this.rb_Galaxia.UseVisualStyleBackColor = true;
+            this.rb_Galaxia.CheckedChanged += new System.EventHandler(this.rb_Galaxia_CheckedChanged);
+            // 
+            // btn_Pos1
+            // 
+            this.btn_Pos1.Location = new System.Drawing.Point(12, 79);
+            this.btn_Pos1.Name = "btn_Pos1";
+            this.btn_Pos1.Size = new System.Drawing.Size(55, 23);
+            this.btn_Pos1.TabIndex = 1;
+            this.btn_Pos1.Text = "Pos 1";
+            this.btn_Pos1.UseVisualStyleBackColor = true;
+            this.btn_Pos1.Click += new System.EventHandler(this.btn_Pos1_Click);
+            // 
+            // btn_Pos2
+            // 
+            this.btn_Pos2.Location = new System.Drawing.Point(12, 108);
+            this.btn_Pos2.Name = "btn_Pos2";
+            this.btn_Pos2.Size = new System.Drawing.Size(55, 23);
+            this.btn_Pos2.TabIndex = 2;
+            this.btn_Pos2.Text = "Pos 2";
+            this.btn_Pos2.UseVisualStyleBackColor = true;
+            this.btn_Pos2.Click += new System.EventHandler(this.btn_Pos2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 249);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Tempo de Click";
+            // 
+            // cb_clickTempo
+            // 
+            this.cb_clickTempo.FormattingEnabled = true;
+            this.cb_clickTempo.Location = new System.Drawing.Point(13, 265);
+            this.cb_clickTempo.Name = "cb_clickTempo";
+            this.cb_clickTempo.Size = new System.Drawing.Size(55, 21);
+            this.cb_clickTempo.TabIndex = 4;
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Location = new System.Drawing.Point(104, 263);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(75, 23);
+            this.btn_Start.TabIndex = 5;
+            this.btn_Start.Text = "AutoClick";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // timerPoint1
+            // 
+            this.timerPoint1.Tick += new System.EventHandler(this.timerPoint1_Tick);
+            // 
+            // timerPoint2
+            // 
+            this.timerPoint2.Tick += new System.EventHandler(this.timerPoint2_Tick);
+            // 
+            // lbl_P1
+            // 
+            this.lbl_P1.AutoSize = true;
+            this.lbl_P1.Location = new System.Drawing.Point(84, 325);
+            this.lbl_P1.Name = "lbl_P1";
+            this.lbl_P1.Size = new System.Drawing.Size(35, 13);
+            this.lbl_P1.TabIndex = 6;
+            this.lbl_P1.Text = "lable1";
+            // 
+            // lbl_P2
+            // 
+            this.lbl_P2.AutoSize = true;
+            this.lbl_P2.Location = new System.Drawing.Point(83, 348);
+            this.lbl_P2.Name = "lbl_P2";
+            this.lbl_P2.Size = new System.Drawing.Size(39, 13);
+            this.lbl_P2.TabIndex = 7;
+            this.lbl_P2.Text = "Lable2";
+            // 
+            // btn_pos3
+            // 
+            this.btn_pos3.Location = new System.Drawing.Point(12, 138);
+            this.btn_pos3.Name = "btn_pos3";
+            this.btn_pos3.Size = new System.Drawing.Size(55, 23);
+            this.btn_pos3.TabIndex = 8;
+            this.btn_pos3.Text = "Pos 3";
+            this.btn_pos3.UseVisualStyleBackColor = true;
+            this.btn_pos3.Click += new System.EventHandler(this.btn_pos3_Click);
+            // 
+            // lbl_avancaSS
+            // 
+            this.lbl_avancaSS.AutoSize = true;
+            this.lbl_avancaSS.Location = new System.Drawing.Point(76, 84);
+            this.lbl_avancaSS.Name = "lbl_avancaSS";
+            this.lbl_avancaSS.Size = new System.Drawing.Size(101, 13);
+            this.lbl_avancaSS.TabIndex = 9;
+            this.lbl_avancaSS.Text = "Posição avanço SS";
+            // 
+            // lbl_avancaGalax
+            // 
+            this.lbl_avancaGalax.AutoSize = true;
+            this.lbl_avancaGalax.Location = new System.Drawing.Point(79, 117);
+            this.lbl_avancaGalax.Name = "lbl_avancaGalax";
+            this.lbl_avancaGalax.Size = new System.Drawing.Size(95, 13);
+            this.lbl_avancaGalax.TabIndex = 10;
+            this.lbl_avancaGalax.Text = "Posição avanço G";
+            // 
+            // lbl_posBtnVer
+            // 
+            this.lbl_posBtnVer.AutoSize = true;
+            this.lbl_posBtnVer.Location = new System.Drawing.Point(80, 148);
+            this.lbl_posBtnVer.Name = "lbl_posBtnVer";
+            this.lbl_posBtnVer.Size = new System.Drawing.Size(94, 13);
+            this.lbl_posBtnVer.TabIndex = 11;
+            this.lbl_posBtnVer.Text = "Posição Botão ver";
+            // 
+            // btn_pos4
+            // 
+            this.btn_pos4.Location = new System.Drawing.Point(12, 168);
+            this.btn_pos4.Name = "btn_pos4";
+            this.btn_pos4.Size = new System.Drawing.Size(55, 23);
+            this.btn_pos4.TabIndex = 12;
+            this.btn_pos4.Text = "Pos 4";
+            this.btn_pos4.UseVisualStyleBackColor = true;
+            this.btn_pos4.Click += new System.EventHandler(this.btn_pos4_Click);
+            // 
+            // lbl_recuaSS
+            // 
+            this.lbl_recuaSS.AutoSize = true;
+            this.lbl_recuaSS.Location = new System.Drawing.Point(79, 177);
+            this.lbl_recuaSS.Name = "lbl_recuaSS";
+            this.lbl_recuaSS.Size = new System.Drawing.Size(100, 13);
+            this.lbl_recuaSS.TabIndex = 13;
+            this.lbl_recuaSS.Text = "Posição Recuar SS";
+            // 
+            // lbl_actualizaGalax
+            // 
+            this.lbl_actualizaGalax.AutoSize = true;
+            this.lbl_actualizaGalax.Location = new System.Drawing.Point(11, 215);
+            this.lbl_actualizaGalax.Name = "lbl_actualizaGalax";
+            this.lbl_actualizaGalax.Size = new System.Drawing.Size(105, 13);
+            this.lbl_actualizaGalax.TabIndex = 14;
+            this.lbl_actualizaGalax.Text = "Actualizar n Galaxias";
+            // 
+            // cb_actualizaGalax
+            // 
+            this.cb_actualizaGalax.FormattingEnabled = true;
+            this.cb_actualizaGalax.Location = new System.Drawing.Point(122, 212);
+            this.cb_actualizaGalax.Name = "cb_actualizaGalax";
+            this.cb_actualizaGalax.Size = new System.Drawing.Size(49, 21);
+            this.cb_actualizaGalax.TabIndex = 15;
+            this.cb_actualizaGalax.SelectedIndexChanged += new System.EventHandler(this.cb_actualizaGalax_SelectedIndexChanged);
+            // 
+            // timePoint3
+            // 
+            this.timePoint3.Tick += new System.EventHandler(this.timePoint3_Tick);
+            // 
+            // timerPoint4
+            // 
+            this.timerPoint4.Tick += new System.EventHandler(this.timerPoint4_Tick);
+            // 
+            // timerAvancaSS
+            // 
+            this.timerAvancaSS.Tick += new System.EventHandler(this.timerAvancaSS_Tick);
+            // 
+            // timerRecuaSS
+            // 
+            this.timerRecuaSS.Tick += new System.EventHandler(this.timerRecuaSS_Tick);
+            // 
+            // timerAvancaGalaxia
+            // 
+            this.timerAvancaGalaxia.Tick += new System.EventHandler(this.timerAvancaGalaxia_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApplication1.Properties.Resources.UP3;
+            this.pictureBox1.Location = new System.Drawing.Point(16, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(51, 51);
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(187, 382);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.cb_actualizaGalax);
+            this.Controls.Add(this.lbl_actualizaGalax);
+            this.Controls.Add(this.lbl_recuaSS);
+            this.Controls.Add(this.btn_pos4);
+            this.Controls.Add(this.lbl_posBtnVer);
+            this.Controls.Add(this.lbl_avancaGalax);
+            this.Controls.Add(this.lbl_avancaSS);
+            this.Controls.Add(this.btn_pos3);
+            this.Controls.Add(this.lbl_P2);
+            this.Controls.Add(this.lbl_P1);
+            this.Controls.Add(this.btn_Start);
+            this.Controls.Add(this.cb_clickTempo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_Pos2);
+            this.Controls.Add(this.btn_Pos1);
+            this.Controls.Add(this.groupBox1);
+            this.DoubleBuffered = true;
+            this.Location = new System.Drawing.Point(1080, 110);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rb_Universo;
+        private System.Windows.Forms.RadioButton rb_Galaxia;
+        private System.Windows.Forms.Button btn_Pos1;
+        private System.Windows.Forms.Button btn_Pos2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cb_clickTempo;
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.Timer timerPoint1;
+        private System.Windows.Forms.Timer timerPoint2;
+        private System.Windows.Forms.Label lbl_P1;
+        private System.Windows.Forms.Label lbl_P2;
+        private System.Windows.Forms.Button btn_pos3;
+        private System.Windows.Forms.Label lbl_avancaSS;
+        private System.Windows.Forms.Label lbl_avancaGalax;
+        private System.Windows.Forms.Label lbl_posBtnVer;
+        private System.Windows.Forms.Button btn_pos4;
+        private System.Windows.Forms.Label lbl_recuaSS;
+        private System.Windows.Forms.Label lbl_actualizaGalax;
+        private System.Windows.Forms.ComboBox cb_actualizaGalax;
+        private System.Windows.Forms.Timer timePoint3;
+        private System.Windows.Forms.Timer timerPoint4;
+        private System.Windows.Forms.Timer timerAvancaSS;
+        private System.Windows.Forms.Timer timerRecuaSS;
+        private System.Windows.Forms.Timer timerPressionaBTver;
+        private System.Windows.Forms.Timer timerAvancaGalaxia;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,360 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        //constantes Mouse Event
+        const int MOUSEEVENTF_LEFTDOWN = 2;
+        const int MOUSEEVENTF_LEFTUP = 4;
+        const int MOUSEEVENTF_RIGHTDOWN = 8;
+        const int MOUSEEVENTF_RIGHT_UP = 16;
+
+        const int SISTEMAS = 499;
+        const int GALAXIAS = 10;
+        public int restoSistemas = 0;
+        public int nClicks = 495;
+        public int nGalaxias = 0;
+        public int vGalaxias = 0;
+
+
+
+
+
+        //input type constant
+        const int INPUT_MOUSE = 0;
+
+        [DllImport("User32.dll", SetLastError = true)]
+        public static extern int SendInput(int nInputs, ref INPUT pInputs, int cbSize);
+
+        Point clickLocationP1;
+        Point clickLocationP2;
+        Point clickLocationP3;
+        Point clickLocationP4;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rb_Galaxia.Select();
+            PopulateComboTempo();
+            PopulateComboGalaxias();
+        }
+
+        private void btn_Pos1_Click(object sender, EventArgs e)
+        {
+            timerPoint1.Interval = 5000;
+            timerPoint1.Start();
+            
+        }
+
+        private void btn_Pos2_Click(object sender, EventArgs e)
+        {
+            timerPoint2.Interval = 5000;
+            timerPoint2.Start();
+        }
+
+        private void timerPoint1_Tick(object sender, EventArgs e)
+        {
+            clickLocationP1 = Cursor.Position;
+            lbl_avancaSS.Text = clickLocationP1.ToString();
+            timerPoint1.Stop();
+        }
+
+        private void timerPoint2_Tick(object sender, EventArgs e)
+        {
+            clickLocationP2 = Cursor.Position;
+            lbl_avancaGalax.Text = clickLocationP2.ToString();
+            timerPoint2.Stop();
+        }
+
+        public struct MOUSEINPUT
+        {
+            public int dx;
+            public int dy;
+            public int mouseData;
+            public int dwFlags;
+            public int time;
+            public IntPtr dwExtraInfo;
+        }
+
+        public struct INPUT
+        {
+            public uint type;
+            public MOUSEINPUT mi;
+        };
+
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            restoSistemas = 0;
+            nClicks = 0;
+            nGalaxias = cb_actualizaGalax.SelectedIndex + 1;
+
+            int iInterval = RetornaComboTempo();
+            if (rb_Galaxia.Checked == true)
+            {
+
+                if (btn_Start.Text == "AutoClick")
+                {
+                    btn_Start.Text = "STOP";
+                    timerAvancaSS.Interval = iInterval;
+                    timerAvancaSS.Start();
+                }
+                else
+                {
+                    
+                    btn_Start.Text = "AutoClick";
+                    timerAvancaSS.Stop();
+                    timerRecuaSS.Stop();
+                    timerAvancaGalaxia.Stop();
+                }
+            }
+            else
+            {
+                if(btn_Start.Text == "AutoClick")
+                {
+                    timerAvancaSS.Interval = iInterval;
+                    timerAvancaSS.Start();
+                    btn_Start.Text = "STOP";
+
+                    
+                    
+                }
+                else
+                {
+                    btn_Start.Text = "AutoClick";
+                    timerAvancaSS.Stop();
+                    timerRecuaSS.Stop();
+                    timerAvancaGalaxia.Stop();
+                }
+             }
+        }
+
+        
+
+        
+
+        private void btn_stop_Click(object sender, EventArgs e)
+        {
+            timerAvancaGalaxia.Stop();
+            timerAvancaSS.Stop();
+            timerRecuaSS.Stop();
+        }
+
+        private void rb_Galaxia_CheckedChanged(object sender, EventArgs e)
+        {
+            btn_Pos2.Enabled = false;
+            btn_pos3.Enabled = false;
+            btn_pos4.Enabled = false;
+            lbl_avancaGalax.Enabled = false;
+            lbl_posBtnVer.Enabled = false;
+            lbl_recuaSS.Enabled = false;
+            lbl_actualizaGalax.Enabled = false;
+            cb_actualizaGalax.Enabled = false;
+        }
+
+        private void rb_Universo_CheckedChanged(object sender, EventArgs e)
+        {
+            btn_Pos2.Enabled = true;
+            btn_pos3.Enabled = true;
+            btn_pos4.Enabled = true;
+            lbl_avancaGalax.Enabled = true;
+            lbl_posBtnVer.Enabled = true;
+            lbl_recuaSS.Enabled = true;
+            lbl_actualizaGalax.Enabled = true;
+
+            cb_actualizaGalax.Enabled = true;
+        }
+
+        // preencher combo segundos
+        void PopulateComboTempo()
+        {
+            for (int n = 1; n < 11; n++)
+            {
+                cb_clickTempo.Items.Add(n + " Sec");
+            }
+
+            cb_clickTempo.SelectedIndex = 4;
+        }
+
+        void PopulateComboGalaxias()
+        {
+            for (int n = 1; n < 11; n++)
+            {
+                cb_actualizaGalax.Items.Add(n);
+            }
+
+            cb_actualizaGalax.SelectedIndex = 1;
+        }
+
+        // utiliza o índice da combo para calculo dos milisegundos
+        int RetornaComboTempo()
+        {
+            int itemSelected = 0;
+
+            itemSelected = ((int)cb_clickTempo.SelectedIndex + 1) * 1000;
+
+            return itemSelected;
+        }
+
+        private void timePoint3_Tick(object sender, EventArgs e)
+        {
+            clickLocationP3 = Cursor.Position;
+            lbl_posBtnVer.Text = clickLocationP3.ToString();
+            timePoint3.Stop();
+        }
+
+        private void timerPoint4_Tick(object sender, EventArgs e)
+        {
+            clickLocationP4 = Cursor.Position;
+            lbl_recuaSS.Text = clickLocationP4.ToString();
+            timerPoint4.Stop();
+        }
+
+        private void cb_actualizaGalax_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_pos3_Click(object sender, EventArgs e)
+        {
+            timePoint3.Interval = 5000;
+            timePoint3.Start();
+        }
+
+        private void btn_pos4_Click(object sender, EventArgs e)
+        {
+            timerPoint4.Interval = 5000;
+            timerPoint4.Start();
+        }
+
+   
+        private void timerAvancaSS_Tick(object sender, EventArgs e)
+        {
+            //set cursor position to memorized location
+            Cursor.Position = clickLocationP1;
+            //set up the INPUT struct and fill it for the mouse down
+            INPUT i = new INPUT();
+            i.type = INPUT_MOUSE;
+            i.mi.dx = 0; //clickLocation.X;
+            i.mi.dy = 0; // clickLocation.Y;
+            i.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+            i.mi.dwExtraInfo = IntPtr.Zero;
+            i.mi.mouseData = 0;
+            i.mi.time = 0;
+            //send the input
+            SendInput(1, ref i, Marshal.SizeOf(i));
+            //set the INPUT for mouse up and send
+            i.mi.dwFlags = MOUSEEVENTF_LEFTUP;
+            SendInput(1, ref i, Marshal.SizeOf(i));
+
+            nClicks++;
+
+            restoSistemas = SISTEMAS - nClicks;
+            //lbl_P1.Text = restoSistemas.ToString();
+            lbl_P1.Text = nClicks.ToString();
+
+            if (nClicks == 499)
+            {
+                timerAvancaSS.Stop();
+                int iInterval = RetornaComboTempo();
+                timerAvancaGalaxia.Interval = iInterval;
+                timerAvancaGalaxia.Start();
+
+            }
+        }
+
+        private void timerRecuaSS_Tick(object sender, EventArgs e)
+        {
+            //set cursor position to memorized location
+            Cursor.Position = clickLocationP4;
+            //set up the INPUT struct and fill it for the mouse down
+            INPUT i = new INPUT();
+            i.type = INPUT_MOUSE;
+            i.mi.dx = 0; //clickLocation.X;
+            i.mi.dy = 0; // clickLocation.Y;
+            i.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+            i.mi.dwExtraInfo = IntPtr.Zero;
+            i.mi.mouseData = 0;
+            i.mi.time = 0;
+            //send the input
+            SendInput(1, ref i, Marshal.SizeOf(i));
+            //set the INPUT for mouse up and send
+            i.mi.dwFlags = MOUSEEVENTF_LEFTUP;
+            SendInput(1, ref i, Marshal.SizeOf(i));
+
+            nClicks--;
+
+            restoSistemas = -SISTEMAS + nClicks;
+            lbl_P2.Text = nClicks.ToString();
+
+            if (nClicks == 0)
+            {
+                timerRecuaSS.Stop();
+                int iInterval = RetornaComboTempo();
+                timerAvancaGalaxia.Interval = iInterval;
+                timerAvancaGalaxia.Start();
+            }
+
+        }
+
+        private void timerAvancaGalaxia_Tick(object sender, EventArgs e)
+        {
+
+            //set cursor position to memorized location
+            Cursor.Position = clickLocationP2;
+            //set up the INPUT struct and fill it for the mouse down
+            INPUT i = new INPUT();
+            i.type = INPUT_MOUSE;
+            i.mi.dx = 0; //clickLocation.X;
+            i.mi.dy = 0; // clickLocation.Y;
+            i.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+            i.mi.dwExtraInfo = IntPtr.Zero;
+            i.mi.mouseData = 0;
+            i.mi.time = 0;
+            //send the input
+            SendInput(1, ref i, Marshal.SizeOf(i));
+            //set the INPUT for mouse up and send
+            i.mi.dwFlags = MOUSEEVENTF_LEFTUP;
+            SendInput(1, ref i, Marshal.SizeOf(i));
+
+            vGalaxias++;
+
+           int iInterval = RetornaComboTempo();
+
+            if (vGalaxias == nGalaxias)
+            {
+                timerAvancaGalaxia.Stop();
+                MessageBox.Show("Parou!!!");
+            }
+            else if (vGalaxias % 2 != 0)
+            {
+                timerAvancaGalaxia.Stop();
+                timerRecuaSS.Interval = iInterval;
+                timerRecuaSS.Start();
+            }
+            else
+            {
+                timerAvancaGalaxia.Stop();
+                timerAvancaSS.Interval = iInterval;
+                timerAvancaSS.Start();
+            }
+        }
+
+
+
+
+    }
+}
